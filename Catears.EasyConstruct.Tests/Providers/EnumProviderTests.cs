c949219a82b86f0a8157c89f2ce69188aebcfb62
Henrik 'Catears' Adolfsson <catears13@gmail.com>
2022-11-22 22:28:10 +0100

    Add tests for enum provider

---
+++ Catears.EasyConstruct.Tests/Providers/EnumProviderTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Catears.EasyConstruct.Providers;
+using Xunit;
+
+namespace Catears.EasyConstruct.Tests.Providers;
+
+internal enum TestableEnum
+{
+    A,
+    B,
+    C,
+    D,
+    E
+}
+
+public class EnumProviderTests
+{
+
+    [Fact]
+    public void RandomEnum_WhenCalled1000Times_ProducesEachEnumAtLeastOnce()
+    {
+        const int iterations = 1000;
+        var expectedEnums = Enum.GetValues<TestableEnum>().ToHashSet();
+        var generatedEnums = new HashSet<TestableEnum>();
+        var sut = new EnumProvider();
+        
+        for (var idx = 0; idx < iterations; ++idx)
+        {
+            generatedEnums.Add((TestableEnum) sut.RandomEnum<TestableEnum>());
+        }
+        
+        Assert.Equal(expectedEnums, generatedEnums);
+        Assert.True(generatedEnums.Count > 0);
+    }
+
+    [Fact]
+    public void RandomEnum_WhenCalledWithNonEnumType_ThrowsAssertionError()
+    {
+        var sut = new EnumProvider();
+
+        Assert.Throws<ArgumentException>(() => sut.RandomEnum(typeof(EnumProvider)));
+    }
+    
+}
