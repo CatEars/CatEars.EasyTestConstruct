07b1fdfbe967e860fd46f25eaadeeca5dff605a1
Henrik 'Catears' Adolfsson <catears13@gmail.com>
2022-11-22 23:34:41 +0100

    Add tests for IntResolver

---
+++ Catears.EasyConstruct.Tests/Resolvers/IntResolverTests.cs
@@ -0,0 +1,29 @@
+﻿using Catears.EasyConstruct.Resolvers;
+using Catears.EasyConstruct.Tests.Resolvers.Fixtures;
+using FakeItEasy;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Catears.EasyConstruct.Tests.Resolvers;
+
+public class IntResolverTests : IClassFixture<BasicProviderFixture>
+{
+    private BasicProviderFixture Fixture { get; }
+    
+    public IntResolverTests(BasicProviderFixture fixture)
+    {
+        Fixture = fixture;
+    }
+
+    [Fact]
+    public void ResolveParameter_ReturnsInt()
+    {
+        var sut = new IntResolver();
+        using var scope = Fixture.ServiceCollection.BuildServiceProvider();
+
+        var result = sut.ResolveParameter(scope);
+
+        Assert.IsType<int>(result);
+    }
+
+}
