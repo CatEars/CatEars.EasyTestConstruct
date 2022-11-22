f00f6f321867fb8523bc4fb4f721baeeeb93f123
Henrik 'Catears' Adolfsson <catears13@gmail.com>
2022-11-22 23:40:42 +0100

    Add tests for string resolver

---
+++ Catears.EasyConstruct.Tests/Resolvers/StringResolverTests.cs
@@ -0,0 +1,48 @@
+﻿using Catears.EasyConstruct.Providers;
+using Catears.EasyConstruct.Resolvers;
+using Catears.EasyConstruct.Tests.Resolvers.Fixtures;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Catears.EasyConstruct.Tests.Resolvers;
+
+public class StringResolverTests : IClassFixture<BasicProviderFixture>
+{
+    private BasicProviderFixture Fixture { get; }
+
+    public StringResolverTests(BasicProviderFixture fixture)
+    {
+        Fixture = fixture;
+    }
+
+    [Fact]
+    public void ResolveParameter_WithNoConfiguration_GeneratesString()
+    {
+        var sut = new StringResolver(StringProviderOptions.Default);
+        using var scope = Fixture.ServiceCollection.BuildServiceProvider();
+
+        var result = sut.ResolveParameter(scope);
+
+        Assert.IsType<string>(result);
+        Assert.False(string.IsNullOrWhiteSpace((string) result));
+    }
+
+    [Fact]
+    public void ResolveParameter_WithVariableAndTypeName_GeneratesStringContainingVariableAndTypeName()
+    {
+        var sut = new StringResolver(StringProviderOptions.Default with
+        {
+            VariableName = "MyVariableName",
+            VariableType = "MyVariableType"
+        });
+        using var scope = Fixture.ServiceCollection.BuildServiceProvider();
+
+        var result = sut.ResolveParameter(scope);
+
+        Assert.IsType<string>(result);
+        var str = (string) result;
+        Assert.Contains("MyVariableName", str);
+        Assert.Contains("MyVariableType", str);
+    }
+    
+}
