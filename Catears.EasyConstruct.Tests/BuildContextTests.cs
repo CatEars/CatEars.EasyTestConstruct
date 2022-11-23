e51cd9e8817106dcedcadf5ed590bb00e5ee1df7
Henrik 'Catears' Adolfsson <catears13@gmail.com>
2022-11-23 22:25:37 +0100

    Add tests to reach 100% coverage

---
+++ Catears.EasyConstruct.Tests/BuildContextTests.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+
+namespace Catears.EasyConstruct.Tests;
+
+public class BuildContextTests
+{
+
+    private record SampleRecord(string StringValue);
+    
+    [Fact]
+    public void Register_WithBuilderFunc_RegistersSampleRecord()
+    {
+        var buildContext = new BuildContext();
+        
+        buildContext.Register(() => new SampleRecord(""));
+
+        using var scope = buildContext.Scope();
+        Assert.Equal(new SampleRecord(""), scope.Resolve<SampleRecord>());
+    }
+    
+    [Fact]
+    public void Register_WithBuilderFuncUsingProvider_RegistersSampleRecord()
+    {
+        var buildContext = new BuildContext();
+        
+        buildContext.Register(_ => new SampleRecord(""));
+
+        using var scope = buildContext.Scope();
+        Assert.Equal(new SampleRecord(""), scope.Resolve<SampleRecord>());
+    }
+    
+}
