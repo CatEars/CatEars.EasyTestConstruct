fc3f08609ef71980b40e9860615bc25a414b0157
Henrik 'Catears' Adolfsson <catears13@gmail.com>
2022-11-23 19:43:52 +0100

    Add tests for calling basic providers through service provider

---
+++ Catears.EasyConstruct.Tests/Providers/Fixtures/BasicProvidersFixture.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Catears.EasyConstruct.Providers;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Catears.EasyConstruct.Tests.Providers.Fixtures;
+
+public class BasicProvidersFixture
+{
+    public IServiceProvider ServiceProvider { get; }
+    
+    public BasicProvidersFixture()
+    {
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.RegisterBasicValueProviders();
+        ServiceProvider = serviceCollection.BuildServiceProvider();
+    }    
+    
+}
---
+++ Catears.EasyConstruct.Tests/Providers/ServiceProviderExtensionsTests.cs
@@ -0,0 +1,57 @@
+﻿using Catears.EasyConstruct.Providers;
+using Catears.EasyConstruct.Tests.Providers.Fixtures;
+using Xunit;
+
+namespace Catears.EasyConstruct.Tests.Providers;
+
+public class ServiceProviderExtensionsTests : IClassFixture<BasicProvidersFixture>
+{
+    private BasicProvidersFixture Fixture { get; }
+
+    private enum TestEnum
+    {
+        A, B, C
+    }
+    
+    public ServiceProviderExtensionsTests(BasicProvidersFixture fixture)
+    {
+        Fixture = fixture;
+    }
+
+    [Fact]
+    public void RandomInt_ReturnsAnInt()
+    {
+        var randomInt = Fixture.ServiceProvider.RandomInt();
+        
+        Assert.IsType<int>(randomInt);
+    }
+
+    [Fact]
+    public void RandomEnum_ReturnsAnEnum()
+    {
+        var randomEnum = Fixture.ServiceProvider.RandomEnum(typeof(TestEnum));
+        Assert.IsType<TestEnum>(randomEnum);
+    }
+
+    [Fact]
+    public void RandomEnum_WithTypeParameter_ReturnsAnEnum()
+    {
+        var randomEnum = Fixture.ServiceProvider.RandomEnum<TestEnum>();
+        Assert.IsType<TestEnum>(randomEnum);
+    }
+
+    [Fact]
+    public void RandomString_ReturnsAString()
+    {
+        var randomString = Fixture.ServiceProvider.RandomString();
+        Assert.IsType<string>(randomString);
+    }
+
+    [Fact]
+    public void RandomString_WithParameters_ReturnsAStringMatchingOptions()
+    {
+        var randomString = Fixture.ServiceProvider.RandomString(new StringProviderOptions("AAAA", "BBBB"));
+        Assert.Contains("AAAA", randomString);
+        Assert.Contains("BBBB", randomString);
+    }
+}
