c12c61b9750be258a656a4e11f1992f0e066d497
Henrik 'Catears' Adolfsson <catears13@gmail.com>
2022-11-23 22:21:55 +0100

    Create tests for BuildScope

---
+++ Catears.EasyConstruct.Tests/BuildScopeTests.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Catears.EasyConstruct.Tests;
+
+public class BuildScopeTests
+{
+    private record SampleRecord(string StringValue);
+
+    [Fact]
+    public void Memoize_WithTypeParameter_MemoizesThatClass()
+    {
+        using var scope = CreateSampleBuildScope();
+        scope.Memoize<SampleRecord>();
+
+        var firstResult = scope.Resolve<SampleRecord>();
+        var secondResult = scope.Resolve<SampleRecord>();
+        
+        Assert.Same(firstResult, secondResult);
+    }
+
+    [Fact]
+    public void Memoize_CalledMultipleTimes_WillStillMemoize()
+    {
+        using var scope = CreateSampleBuildScope();
+        scope.Memoize<SampleRecord>();
+        scope.Memoize<SampleRecord>();
+        scope.Memoize<SampleRecord>();
+        scope.Memoize<SampleRecord>();
+        scope.Memoize<SampleRecord>();
+
+        var firstResult = scope.Resolve<SampleRecord>();
+        var secondResult = scope.Resolve<SampleRecord>();
+        
+        Assert.Same(firstResult, secondResult);
+    }
+
+    [Fact]
+    public void Use_CalledWithBasicBuilderFunction_UsesBasicBuilderFunction()
+    {
+        using var scope = CreateSampleBuildScope();
+        var count = 0;
+        scope.Use(() =>
+        {
+            ++count;
+            return new object();
+        });
+
+        scope.Resolve<object>();
+        
+        Assert.Equal(1, count);
+    }
+
+    [Fact]
+    public void Memoize_WhenUsingSingleton_ResolvesSingletonObject()
+    {
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddSingleton(new object());
+        using var buildScope = new BuildScope(serviceCollection);
+        buildScope.Memoize<object>();
+        
+        var firstResult = buildScope.Resolve<object>();
+        var secondResult = buildScope.Resolve<object>();
+        
+        Assert.NotNull(firstResult);
+        Assert.Same(firstResult, secondResult);
+    }
+
+    private abstract class SampleAbstractClass
+    {
+    }
+
+    [Fact]
+    public void Memoize_WhenPoorlyConfigured_ThrowsInvalidOperationException()
+    {
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddSingleton<SampleAbstractClass>();
+        using var buildScope = new BuildScope(serviceCollection);
+
+        Assert.Throws<InvalidOperationException>(() => buildScope.Memoize<SampleAbstractClass>());
+    }
+    
+    private static IBuildScope CreateSampleBuildScope()
+    {
+        var buildContext = new BuildContext();
+        buildContext.Register<SampleRecord>();
+        return buildContext.Scope();
+    }
+}
