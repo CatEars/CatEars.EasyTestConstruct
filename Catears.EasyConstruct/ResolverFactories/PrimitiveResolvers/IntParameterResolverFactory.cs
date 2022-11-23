01c8a0acae692f86be85299e0e8b1ab53f12eb20
Henrik 'Catears' Adolfsson <catears13@gmail.com>
2022-11-23 22:44:47 +0100

    Change IntParameterResolverFactory into using an abstract base class

--- Catears.EasyConstruct/ResolverFactories/PrimitiveResolvers/IntParameterResolverFactory.cs
+++ Catears.EasyConstruct/ResolverFactories/PrimitiveResolvers/IntParameterResolverFactory.cs
@@ -1,20 +1,5 @@
-﻿using System.Reflection;
-using Catears.EasyConstruct.Resolvers;
+﻿using Catears.EasyConstruct.Resolvers;
 
 namespace Catears.EasyConstruct.ResolverFactories.PrimitiveResolvers;
 
-internal class IntParameterResolverFactory : IParameterResolverFactory
-{
-    public bool TryCreateParameterResolver(ParameterInfo parameterInfo, out IParameterResolver? resolver)
-    {
-        var type = parameterInfo.ParameterType;
-        if (type == typeof(int) || type == typeof(Int32))
-        {
-            resolver = new IntResolver();
-            return true;
-        }
-
-        resolver = null;
-        return false;
-    }
-}
+internal class IntParameterResolverFactory : PrimitiveParameterResolverFactory<int, IntResolver> {}
---
+++ Catears.EasyConstruct/ResolverFactories/PrimitiveResolvers/PrimitiveParameterResolverFactory.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using Catears.EasyConstruct.Resolvers;
+
+namespace Catears.EasyConstruct.ResolverFactories.PrimitiveResolvers;
+
+internal abstract class PrimitiveParameterResolverFactory<TExpectedType, TExpectedResolver> : IParameterResolverFactory where TExpectedResolver : IParameterResolver, new()
+{
+    public bool TryCreateParameterResolver(ParameterInfo parameterInfo, out IParameterResolver? resolver)
+    {
+        if (parameterInfo.ParameterType == typeof(TExpectedType))
+        {
+            resolver = new TExpectedResolver();
+            return true;
+        }
+
+        resolver = null;
+        return false;
+    }
+}
