54418abdeb9d15f917b11dac8607602589b4a700
Henrik 'Catears' Adolfsson <catears13@gmail.com>
2022-11-23 19:19:54 +0100

    Add Tests for memoized resolver

---
+++ Catears.EasyConstruct.Tests/Resolvers/MemoizedResolverTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Catears.EasyConstruct.Resolvers;
+using Catears.EasyConstruct.Tests.Resolvers.Fixtures;
+using FakeItEasy;
+using Xunit;
+
+namespace Catears.EasyConstruct.Tests.Resolvers;
+
+public class MemoizedResolverTests : IClassFixture<BasicProviderFixture>
+{
+    private BasicProviderFixture Fixture { get; }
+
+    public MemoizedResolverTests(BasicProviderFixture fixture)
+    {
+        Fixture = fixture;
+    }
+
+    [Fact]
+    public void ResolveParameter_WhenCalledOnce_ReturnsObjectConstructedByBuilder()
+    {
+        var obj = new object();
+        object Builder(IServiceProvider _) => obj;
+        var resolver = new MemoizedResolver(Builder);
+
+        var result = resolver.ResolveParameter(A.Fake<IServiceProvider>());
+        
+        Assert.Same(obj, result);
+    }
+    
+    [Fact]
+    public void ResolveParameter_WhenCalledTwice_CallsBuilderOnlyOnce()
+    {
+        var count = 0;
+        object Builder(IServiceProvider _)
+        {
+            ++count;
+            return new object();
+        }
+
+        var resolver = new MemoizedResolver(Builder);
+
+        resolver.ResolveParameter(A.Fake<IServiceProvider>());
+        resolver.ResolveParameter(A.Fake<IServiceProvider>());
+        resolver.ResolveParameter(A.Fake<IServiceProvider>());
+        
+        Assert.Equal(1, count);
+    }
+}
