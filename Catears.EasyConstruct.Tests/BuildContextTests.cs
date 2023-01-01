71067a9bebd508ab7e0031a2d741175398b96c43
Henke Adolfsson <catears13@gmail.com>
2023-01-01 22:48:30 +0100

    Add test to reach 100% coverage and check interface registration with default options

--- Catears.EasyConstruct.Tests/BuildContextTests.cs
+++ Catears.EasyConstruct.Tests/BuildContextTests.cs
@@ -1,32 +1,45 @@
-﻿using Xunit;
+﻿using System;
+using Xunit;
 
 namespace Catears.EasyConstruct.Tests;
 
 public class BuildContextTests
 {
-
     private record SampleRecord(string StringValue);
-    
+
+    public interface ITestInterface
+    {
+    }
+
+    public record SampleInterfaceRecord(ITestInterface TestInterface);
+
     [Fact]
     public void Register_WithBuilderFunc_RegistersSampleRecord()
     {
         var buildContext = new BuildContext();
-        
+
         buildContext.Register(() => new SampleRecord(""));
 
         var scope = buildContext.Scope();
         Assert.Equal(new SampleRecord(""), scope.Resolve<SampleRecord>());
     }
-    
+
     [Fact]
     public void Register_WithBuilderFuncUsingProvider_RegistersSampleRecord()
     {
         var buildContext = new BuildContext();
-        
+
         buildContext.Register(_ => new SampleRecord(""));
 
         var scope = buildContext.Scope();
         Assert.Equal(new SampleRecord(""), scope.Resolve<SampleRecord>());
     }
-    
+
+    [Fact]
+    public void Register_WithInterfaceAndDefaultRegistrationOptions_ThrowsArgumentException()
+    {
+        var context = new BuildContext();
+
+        Assert.Throws<ArgumentException>(() => context.Register<ITestInterface>());
+    }
 }
